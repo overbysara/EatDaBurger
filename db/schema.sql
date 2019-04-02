cb647e1d1c20be4830fab3071f5a6e31543c50fa
Sara Overby <saraoverby@Mendys-Air.domain>
2019-04-01 20:58:24 -0600

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,9 @@
-2. In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-   * Create the `burgers_db`.
-   * Switch to or use the `burgers_db`.
-   * Create a `burgers` table with these fields:
-     * **id**: an auto incrementing int that serves as the primary key.
-     * **burger_name**: a string.
-     * **devoured**: a boolean.
+CREATE TABLE burgers
+(
+id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR (255) NOT NULL,
+devoured BOOLEAN, 
+);
